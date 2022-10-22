aec2ce0add4242ff6c0473028ea4ace7c83afa13
shubham-X <56729167+shubham-X@users.noreply.github.com>
2022-10-23 05:09:11 +0530

    Initial build which with all basic features

---
+++ NotePad/src/main/java/com/mycompany/notepad/MyApp.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package com.mycompany.notepad;
+
+/**
+ *
+ * @author bhaiy
+ */
+public class MyApp {
+
+    public static void main(String[] args) {
+        NotePad notepad = new NotePad();
+    }
+}
